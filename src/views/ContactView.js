f7249dea96c481d5934a02448bba1061fafff263
Daniel Öhrn <Daniel.ohrnhasslof@student.kyh.se>
2018-05-11 19:05:01 +0200

    remove unused state from contactview and make ContactView a functional component instead of class

--- src/views/ContactView.js
+++ src/views/ContactView.js
@@ -9,38 +9,31 @@ import EmailIcon from '../components/SVG/EmailIcon';
 
 import '../App.css'; 
 
-export default class ContactView extends Component {
+const ContactView = () => (
+    <section className="flex-container col jc-center al-center">
+        <PageHeader text={'Contact'}/>
 
-    state = {
-        messageSent: false 
-    }
+        <Card width={'80%'} backgroundColor={'white'}>
+            <ul className="">
+                <li style={{display: 'flex', alignItems: 'center', margin: '.5em 0'}}>
+                    <EmailIcon/>
+                    <span style={{paddingLeft: '.5em'}}>dhasslof@gmail.com</span>
+                </li>
+                <li style={{display: 'flex', alignItems: 'center', margin: '.5em 0'}}>
+                        <LinkedInIcon/>
+                        <span style={{paddingLeft: '.5em'}}>
+                            <a target="_blank" 
+                                href="https://www.linkedin.com/in/daniel-%C3%B6hrn-hassl%C3%B6f-a3b1a8a1/">
+                                LinkedIn
+                            </a>
+                        </span>
+                </li>
+            </ul>
+        </Card>
 
-    render(){
-        return (
-            <section className="flex-container col jc-center al-center">
-                <PageHeader text={'Contact'}/>
+        <PageArrow to={'/'}/>
+    </section>
+); 
 
-                <Card width={'80%'} backgroundColor={'white'}>
-                    <ul className="">
-                        <li style={{display: 'flex', alignItems: 'center', margin: '.5em 0'}}>
-                            <EmailIcon/>
-                            <span style={{paddingLeft: '.5em'}}>dhasslof@gmail.com</span>
-                        </li>
-                        <li style={{display: 'flex', alignItems: 'center', margin: '.5em 0'}}>
-                                <LinkedInIcon/>
-                                <span style={{paddingLeft: '.5em'}}>
-                                    <a target="_blank" 
-                                       href="https://www.linkedin.com/in/daniel-%C3%B6hrn-hassl%C3%B6f-a3b1a8a1/">
-                                        LinkedIn
-                                    </a>
-                                </span>
-                        </li>
-                    </ul>
-                </Card>
-
-                <PageArrow to={'/'}/>
-            </section>
-        )
-    }
-}
+export default ContactView; 
 
